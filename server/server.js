31ff371045ee44a5fd6ebb58dbbb3904e16f583c
BHAGYA1642 <gbhagya573@gmail.com>
2025-03-27 15:10:29 +0530

    Added Basic setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
-import cors from 'cors'
-const app=express();
-app.use(express.json())
-app.get('/',(req,res)=>
-{
-    res.send(`<h2>welcome to the server</h2>`)
-})
+import cors from 'cors';
+import ConnectDb from './config/Connect.js'; 
+import 'dotenv/config'
 
-app.listen(3000,()=>
-{
-    console.log(`http://localhost:3000`)
-})
+const app = express();
+app.use(express.json());
+
+ConnectDb();
+
+app.get('/', (req, res) => {
+    res.send(`<h2>Welcome to the server</h2>`);
+});
+
+app.listen(3000, () => {
+    console.log(`http://localhost:3000`);
+});
