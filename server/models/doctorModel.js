5227b317b65d28fa47a74606d9fa833c21ab0b57
BHAGYA1642 <gbhagya573@gmail.com>
2025-03-27 21:25:06 +0530

    success

--- server/models/doctorModel.js
+++ server/models/doctorModel.js
@@ -11,7 +11,8 @@ const doctorSchema = new mongoose.Schema({
   img:{type:String,default:"https://www.google.com/imgres?q=doctor&imgurl=https%3A%2F%2Fwww.shutterstock.com%2Fimage-vector%2Fmale-doctor-smiling-happy-face-600nw-2481032615.jpg&imgrefurl=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fdoctor-character&docid=0gRqjQaWOKSHuM&tbnid=BTIkBfkQ9CwtCM&vet=12ahUKEwiewcv4-qmMAxUZslYBHU9wH3QQM3oECBkQAA..i&w=600&h=600&hcb=2&ved=2ahUKEwiewcv4-qmMAxUZslYBHU9wH3QQM3oECBkQAA"},
   hospitalId:{
       type:mongoose.Schema.Types.ObjectId,
-      ref:'hospital'
+      ref:'hospital',
+      required:true
   }
 });
 
