4e0f52b8d08546af399f17afdc1d02c6e5a6418a
BHAGYA1642 <gbhagya573@gmail.com>
2025-03-28 07:29:57 +0530

    Morning Commit

--- frontend/src/pages/patient/homePage.jsx
+++ frontend/src/pages/patient/homePage.jsx
@@ -38,7 +38,7 @@ const PatientHome = () => {
   const getDoctors=async()=>{
     try {
       const response=await axios.post('http://localhost:3000/hospital/get-by-location',{
-        "pincode":"522403"
+        "pincode":"523002"
     })
     setDoctors(response.data.doctors)
     } catch (error) {
@@ -130,7 +130,7 @@ const PatientHome = () => {
                   className="bg-white/90 rounded-xl p-5 shadow-md hover:shadow-lg transition-shadow flex flex-col h-full"
                 >
                   <div className="p-2 w-12 h-12 rounded-full bg-gray-50 flex items-center justify-center mb-4">
-                    <img src={action.img} alt="" className="rounded-full" />
+                    <img src={action.img} alt="" className="rounded-full " />
                   </div>
                   <h3 className="font-semibold text-gray-800 mb-2">{action.name}</h3>
                   <p className="text-gray-600 text-sm">{action.speciality}</p>
@@ -138,20 +138,17 @@ const PatientHome = () => {
                   <div>
                     {[...Array(action.rating)].map((_, index) => (
                       <span key={index}>⭐</span>
+                      
                     ))}
                   </div>
+                  <button className="w-full bg-amber-200 px-5 py-3 items-center justify-center mt-9 hover:cursor-pointer">Book Appointment</button>
                 </Link>
               ))}
             </div>
           </motion.div>
 
           {/* Health Stats & Notifications */}
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-            
-           
-
          
-          </div>
         </div>
       </div>
     </>
